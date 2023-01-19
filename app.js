9da51574b6eab3dde89ba866c473681a8421e8c7
muhammadtallah1 <115802615+muhammadtallah1@users.noreply.github.com>
2023-01-19 01:44:48 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,51 @@
+// var a = "Tallah";
+// console.log(a);
+// var a ;
+// a = "Sufyan";
+// document.write(a);
+let b = "Sufyan",
+    c = "Tallah";
+console.log(b + " " + c);
+const Mymsg = () =>{
+    let name = document.getElementById("name").value;
+    alert("Hello " + name);
+}
+let x = 5;
+console.log(x);
+x = 3;
+console.log(x);
+let y = "Hi ";
+let Y = "Tallah";
+console.log(y + Y);
+var
+let = "hi";
+console.log(let);
+// const x = "65";
+// let const = "hi";
+// console.log(x);
+
+// let wrong = "Enter Your Email";
+
+// alert(wrong);
+
+// const Mymsg = (name) =>{
+//     alert ("Hello "+name);
+// }
+// const Mymsg = () => {
+//     let name = document.getElementById("name").value;
+//     let fathername = document.getElementById("fathername").value;
+//     let email = document.getElementById("email").value;
+//     let number = document.getElementById("number").value;
+//     if (name == "") {
+//         alert("enter your name");
+//     } else if (fathername == "") {
+//         alert("enter father name");
+//     } else if (email == "") {
+//         alert("enter your email");
+//     } else if (number == "") {
+//         alert("enter your number");
+//     } 
+//     else {
+//         alert("Hello "+name+" "+fathername+" "+email+" "+number);
+//     }
+// }
